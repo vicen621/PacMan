e651d21ca10893f742a8e66de40f7ed49aec6c26
Vicen621 <71749981+vicen621@users.noreply.github.com>
2024-03-03 03:39:17 -0300

    feat: modified how pacman moves and render

--- src/main/java/io/github/vicen621/pieces/characters/PacMan.java
+++ src/main/java/io/github/vicen621/pieces/characters/PacMan.java
@@ -1,7 +1,8 @@
-package io.github.vicen621.pieces;
+package io.github.vicen621.pieces.characters;
 
 import io.github.vicen621.board.Board;
-import io.github.vicen621.board.Wall;
+import io.github.vicen621.pieces.FacedPiece;
+import io.github.vicen621.pieces.Tickable;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
@@ -11,39 +12,29 @@ public class PacMan extends FacedPiece implements Tickable {
     private static final int MOVE_SPEED = 3;
     private Face nextMove;
     private int score;
+    private int moveCounter;
 
     public PacMan() {
-        super("/assets/characters/pacman.png");
+        super("/assets/characters/pacman/pacman.gif");
 
+        this.nextMove = getFace();
         this.score = 0;
+        this.moveCounter = 0;
+        getPos().setLocation(Board.STARTING_POINT);
     }
 
     public void keyPressed(KeyEvent e) {
         // every keyboard get has a certain code. get the value of that code from the
         // keyboard event so that we can compare it to KeyEvent constants
         int key = e.getKeyCode();
-        Point pos = (Point) getPos().clone();
 
         // depending on which arrow key was pressed, we're going to move the player by
         // one whole tile for this input
-        if (key == KeyEvent.VK_UP) {
-            getPos().translate(0, -1);
-        }
-        if (key == KeyEvent.VK_RIGHT) {
-            getPos().translate(1, 0);
-        }
-        if (key == KeyEvent.VK_DOWN) {
-            getPos().translate(0, 1);
-        }
-        if (key == KeyEvent.VK_LEFT) {
-            getPos().translate(-1, 0);
-        }
-
-        // If the player moves towards a wall, return to previous position
-        for (Wall wall : Board.WALLS) {
-            if (wall.isInBounds(getPos())) {
-                getPos().setLocation(pos);
-            }
+        switch (key) {
+            case KeyEvent.VK_UP -> nextMove = Face.UP;
+            case KeyEvent.VK_RIGHT -> nextMove = Face.RIGHT;
+            case KeyEvent.VK_DOWN -> nextMove = Face.DOWN;
+            case KeyEvent.VK_LEFT -> nextMove = Face.LEFT;
         }
     }
 
@@ -52,11 +43,18 @@ public class PacMan extends FacedPiece implements Tickable {
         // this gets called once every tick, before the repainting process happens.
         // so we can do anything needed in here to update the state of the player.
 
-        // prevent the player from moving off the edge of the board vertically
-        if (getPos().y <= 3) {
-            getPos().y = 4;
-        } else if (getPos().y >= Board.ROWS - 3) {
-            getPos().y = Board.ROWS - 4;
+        this.moveCounter += 1;
+        if (this.moveCounter < MOVE_SPEED)
+            return;
+
+        Point pos = (Point) getPos().clone();
+        getPos().translate(nextMove.dx, nextMove.dy);
+
+        if (!Board.checkCollision(getPos()))
+            setFace(nextMove);
+        else {
+            getPos().setLocation(pos);
+            getPos().translate(getFace().dx, getFace().dy);
         }
 
         if (getPos().y == Board.TUNNEL_ROW) {
@@ -65,14 +63,12 @@ public class PacMan extends FacedPiece implements Tickable {
             } else if (getPos().x >= Board.COLUMNS) {
                 getPos().x = 0;
             }
-        } else {
-            // prevent the player from moving off the edge of the board sideways
-            if (getPos().x <= 1) {
-                getPos().x = 1;
-            } else if (getPos().x >= Board.COLUMNS - 1) {
-                getPos().x = Board.COLUMNS - 2;
-            }
         }
+
+        if (Board.checkCollision(getPos()))
+            getPos().setLocation(pos);
+
+        this.moveCounter = 0;
     }
 
     public String getScore() {
